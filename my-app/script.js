8db4a710d15cd667e666583e9388f55d5b61b195
yizic <yizick@gmail.com>
2020-03-20 17:52:43 +0100

    mini TP 2

--- my-app/script.js
+++ my-app/script.js
@@ -1,10 +1,19 @@
-const fs = require('fs')
+const http = require("http");
+const pug = require("pug");
 
+const compiledFunction = pug.compileFile("template.pug");
+const port = 3000;
 
-fs.readFile('./data.csv', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err)
-        return
-    }
-    console.log(data)
-})
+const server = http.createServer((req, res) => {
+    const generatedTemplate = compiledFunction({
+        name: 'Vincent'
+    });
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end(generatedTemplate);
+});
+
+server.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+});
