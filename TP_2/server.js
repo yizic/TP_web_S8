48eadbddde86e81d925c0ec4306e9f02cb2dc11e
yizic <yizick@gmail.com>
2020-03-26 15:05:57 +0100

    fin du premier TP et début du TP2 (26/03/2020)

---
+++ TP_2/server.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs')
+const pug = require("pug");
+var file = process.argv[2];
+
+const compiledFunction = pug.compileFile('./template.pug')
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+if(!file){
+    console.error('file name missing');
+    process.exit(1)
+}
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.get('/data', function (req, res) {
+
+    const generatedTemplate = compiledFunction({
+        name: 'vincent'
+    });
+
+    fs.readFile(file,'utf8', (err,data) => {
+        if (err) {
+            console.error(err)
+            process.exit(1)
+        }
+        var results = data.toString().split(/\r\n|\n/)
+        var tab = '<table>';
+        for (let result of results) {
+            user = result.split(';');
+            tab += `<tr><td>${user[0]}</td><td>${user[1]}</td></tr>`
+        }
+        tab += '<style type="text/css">td { border-bottom: 1px solid #ddd; }</style>'
+        tab += '</table>';
+    
+        res.statusCode = 200;
+        res.setHeader('Content-Type','text/html')
+        res.end(generatedTemplate + tab)
+    })
+});
+
+app.listen(port, () => console.log(`exemple app listening on port ${port}!`));
+
+/*
+-----------------------------------------------
+
+const http = require('http');
+const pug = require("pug");
+const fs = require('fs')
+var file = process.argv[2];
+var content;
+
+if(!file){
+    console.error('file name missing');
+    process.exit(1)
+}
+
+const compiledFunction = pug.compileFile('./template.pug')
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+        const generatedTemplate = compiledFunction({
+            name: 'vincent'
+        });
+
+        fs.readFile(file,'utf8', (err,data) => {
+            if (err) {
+                console.error(err)
+                process.exit(1)
+            }
+            var results = data.toString().split(/\r\n|\n/)
+            var tab = '<table>';
+            for (let result of results) {
+                user = result.split(';');
+                tab += `<tr><td>${user[0]}</td><td>${user[1]}</td></tr>`
+            }
+            tab += '<style type="text/css">td { border-bottom: 1px solid #ddd; }</style>'
+            tab += '</table>';
+
+            
+        res.statusCode = 200;
+        res.setHeader('Content-Type','text/html')
+        res.end(generatedTemplate + tab)
+        })
+    }
+)
+
+server.listen(port, () => {
+    console.log('Server running at port' + port)
+})
+*/
--- my-app/script.js
+++ my-app/script.js
@@ -1,19 +1,44 @@
-const http = require("http");
+const http = require('http');
 const pug = require("pug");
+const fs = require('fs')
+var file = process.argv[2];
+var content;
 
-const compiledFunction = pug.compileFile("template.pug");
+if(!file){
+    console.error('file name missing');
+    process.exit(1)
+}
+
+const compiledFunction = pug.compileFile('./template.pug')
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    const generatedTemplate = compiledFunction({
-        name: 'Vincent'
-    });
+        const generatedTemplate = compiledFunction({
+            name: 'vincent'
+        });
 
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end(generatedTemplate);
-});
+        fs.readFile(file,'utf8', (err,data) => {
+            if (err) {
+                console.error(err)
+                process.exit(1)
+            }
+            var results = data.toString().split(/\r\n|\n/)
+            var tab = '<table>';
+            for (let result of results) {
+                user = result.split(';');
+                tab += `<tr><td>${user[0]}</td><td>${user[1]}</td></tr>`
+            }
+            tab += '<style type="text/css">td { border-bottom: 1px solid #ddd; }</style>'
+            tab += '</table>';
+
+            
+        res.statusCode = 200;
+        res.setHeader('Content-Type','text/html')
+        res.end(generatedTemplate + tab)
+        })
+    }
+)
 
 server.listen(port, () => {
-    console.log(`Server running at port ${port}`);
-});
+    console.log('Server running at port' + port)
+})
